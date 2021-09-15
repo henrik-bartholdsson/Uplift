5009a187bc0932f7c556a1bb9d97819ac7da755c
Henrik Bartholdsson <henrik.bartholdsson@hotmail.com>
2021-09-15 22:12:58 +0200

    Details page for service added.

--- Uplift/Areas/Customer/Controllers/HomeController.cs
+++ Uplift/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -6,8 +7,10 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Extensions;
 using Uplift.Models;
 using Uplift.Models.ViewModels;
+using Uplift.Utility;
 
 namespace Uplift.Controllers
 {
@@ -34,6 +37,34 @@ namespace Uplift.Controllers
 
 
             return View(HomeVM);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var serviceFromDB = _UnitOfWork.Service.GetFirstOrDefault(includeProperties:"Category,Frequency", filter:c=>c.Id == id);
+
+            return View(serviceFromDB);
+        }
+
+        public IActionResult AddToCart(int serviceId)
+        {
+            List<int> sessionList = new List<int>();
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(SD.SessionCart)))
+            {
+                sessionList.Add(serviceId);
+                HttpContext.Session.SetObject(SD.SessionCart, sessionList);
+            }
+            else
+            {
+                sessionList = HttpContext.Session.GetObject<List<int>>(SD.SessionCart);
+                if(!sessionList.Contains(serviceId))
+                {
+                    sessionList.Add(serviceId);
+                    HttpContext.Session.SetObject(SD.SessionCart, sessionList);
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
