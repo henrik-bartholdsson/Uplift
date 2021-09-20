538a79444ae322703b2e2f2e12a8141c9226b579
Henrik Bartholdsson <henrik.bartholdsson@hotmail.com>
2021-09-20 17:45:00 +0200

    Adding index page to display users, and some bug fixes.

--- Uplift/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
+++ Uplift/Areas/Identity/Pages/Account/RegisterConfirmation.cshtml.cs
@@ -13,12 +13,10 @@ namespace Uplift.Areas.Identity.Pages.Account
     public class RegisterConfirmationModel : PageModel
     {
         private readonly UserManager<IdentityUser> _userManager;
-        private readonly IEmailSender _sender;
 
-        public RegisterConfirmationModel(UserManager<IdentityUser> userManager, IEmailSender sender)
+        public RegisterConfirmationModel(UserManager<IdentityUser> userManager)
         {
             _userManager = userManager;
-            _sender = sender;
         }
 
         public string Email { get; set; }
