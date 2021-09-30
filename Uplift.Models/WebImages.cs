e2d02a71d508581e84414edfbf2983387864187d
Henrik Bartholdsson <henrik.bartholdsson@hotmail.com>
2021-09-30 19:26:03 +0200

    Image upload functionality added.

--- Uplift.Models/WebImages.cs
+++ Uplift.Models/WebImages.cs
@@ -10,7 +10,7 @@ namespace Uplift.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        [Required]
+
         public byte[] Picture { get; set; }
     }
 }
